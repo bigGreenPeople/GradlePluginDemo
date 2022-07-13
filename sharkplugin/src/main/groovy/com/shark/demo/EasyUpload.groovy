850cabc31d6e4a1eaf17ef222093af5a1e80d45d
bigGreenPeople <1243596620@qq.com>
2022-07-13 17:34:27 +0800

    init

---
+++ sharkplugin/src/main/groovy/com/shark/demo/EasyUpload.groovy
@@ -0,0 +1,13 @@
+package com.shark.demo
+
+import org.gradle.api.Project
+import org.gradle.api.Plugin
+
+class EasyUpload implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        // 构建逻辑
+        println "Hello.fu jie..........................................................................."
+    }
+}
